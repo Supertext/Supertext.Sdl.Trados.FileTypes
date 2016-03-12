46bc302c6332b3289bb6942a96052639df220eb7
Heinrich Muralt <heinrich@supertext.ch>
2016-03-12 16:14:16 +0100

    Create ExtendedFileReader

---
+++ Supertext.Sdl.Trados.FileType.PoFile.Tests/ExtendedFileReaderTest.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using Supertext.Sdl.Trados.FileType.PoFile.DotNetWrappers;
+
+namespace Supertext.Sdl.Trados.FileType.PoFile.Tests
+{
+    [TestFixture]
+    public class ExtendedFileReaderTest
+    {
+        private const string TestFilePath = "sample_file_ok";
+
+        [Test]
+        public void ReadLines_WhenGettingFirstLine_ReturnsBeginOfFile()
+        {
+            // Arrange
+            var testee = CreateTestee(string.Empty);
+
+            // Act
+            var result = testee.ReadLines(TestFilePath);
+
+            // Assert
+            result.First().Should().Be(LineType.BeginOfFile.ToString());
+        }
+
+        [Test]
+        public void ReadLines_ReturnsAllLinesOfFile()
+        {
+            // Arrange
+            var testString = @"
+line 1
+line 2
+line 3
+";
+            var testee = CreateTestee(testString);
+
+            // Act
+            var result = testee.ReadLines(TestFilePath).ToList();
+
+            // Assert
+            result.FirstOrDefault(line => line == "line 1").Should().NotBeNullOrEmpty();
+            result.FirstOrDefault(line => line == "line 2").Should().NotBeNullOrEmpty();
+            result.FirstOrDefault(line => line == "line 3").Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ReadLines_WhenGettingLastLine_ReturnsEndOfFile()
+        {
+            // Arrange
+            var testString = @"
+line 1
+line 2
+line 3
+";
+            var testee = CreateTestee(testString);
+
+            // Act
+            var result = testee.ReadLines(TestFilePath);
+
+            // Assert
+            result.Last().Should().Be(LineType.EndOfFile.ToString());
+        }
+
+        private ExtendedFileReader CreateTestee(string testString)
+        {
+            var fileHelper = A.Fake<IFileHelper>();
+
+            var streamReaderFake = new StringReaderWrapper(testString);
+            A.CallTo(() => fileHelper.CreateStreamReader(TestFilePath)).Returns(streamReaderFake);
+
+            return new ExtendedFileReader(fileHelper);
+        }
+    }
+}
---
+++ Supertext.Sdl.Trados.FileType.PoFile/ExtendedFileReader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Supertext.Sdl.Trados.FileType.PoFile.DotNetWrappers;
+
+namespace Supertext.Sdl.Trados.FileType.PoFile
+{
+    public interface IExtendedFileReader
+    {
+        IEnumerable<string> ReadLines(string filePath);
+    }
+
+    public class ExtendedFileReader : IExtendedFileReader
+    {
+        private readonly IFileHelper _fileHelper;
+
+        public ExtendedFileReader(IFileHelper fileHelper)
+        {
+            _fileHelper = fileHelper;
+        }
+
+        public IEnumerable<string> ReadLines(string filePath)
+        {
+            var reader = _fileHelper.CreateStreamReader(filePath);
+
+            yield return LineType.BeginOfFile.ToString();
+
+            string currentLine;
+
+            while ((currentLine = reader.ReadLine()) != null)
+            {
+                yield return currentLine;
+            }
+
+            yield return LineType.EndOfFile.ToString();
+        }
+    }
+}
