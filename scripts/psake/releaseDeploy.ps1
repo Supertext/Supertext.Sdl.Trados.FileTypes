4c9f232f7d1ccca45810039cc9e405ef4147cff9
Heinrich Muralt <heinrich@supertext.ch>
2016-05-02 12:40:32 +0200

    Updated psake scripts

--- scripts/psake/releaseDeploy.ps1
+++ scripts/psake/releaseDeploy.ps1
@@ -11,14 +11,14 @@ properties {
   $slnFile = "$baseDir\$projectName.sln"
   $sourceDirs = "$baseDir*"
   $publishDir = "$baseDir\plugin"
+  $publishDirPlugin1 = "$publishDir\$pluginName1"
+  $publishDirPlugin2 = "$publishDir\$pluginName2"
   $pluginPath1 =  "$baseDir\$projectName\bin\$buildConfig\$pluginName1"
   $pluginPath2 =  "$baseDir\$projectName.WinUI\bin\$buildConfig\$pluginName2"
   $appDataRoaming = $env:APPDATA
   $appDataLocal = $env:LOCALAPPDATA
   $deployDir = "$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Packages"
   $pluginFolders = @(
-    "$deployDir\$pluginName1",
-    "$deployDir\$pluginName2",
 	"$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Packages\$pluginName1",
     "$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Packages\$pluginName2",
 	"$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Unpacked\$pluginFolder1",
@@ -51,13 +51,22 @@ task Deploy -depends Clean {
 }
 
 task Clean { 
-  Write-Host "deleting" $publishDir 
-  Remove-Item $publishDir -Force -Recurse -ErrorAction SilentlyContinue
-
+  if(Test-Path  $publishDirPlugin1){
+    Write-Host "deleting" $publishDirPlugin1 
+    Remove-Item $publishDirPlugin1 -Force -Recurse
+  }
+  
+  if(Test-Path  $publishDirPlugin2){
+    Write-Host "deleting" $publishDirPlugin2 
+    Remove-Item $publishDirPlugin2 -Force -Recurse
+  }
+  
   $pluginFolders | ForEach-Object{
     $pluginFolder = $_
-
-    Write-Host "deleting" $pluginFolder
-    Remove-Item $pluginFolder -Force -Recurse -ErrorAction SilentlyContinue 
+	
+	if(Test-Path  $pluginFolder){
+      Write-Host "deleting" $pluginFolder
+      Remove-Item $pluginFolder -Force -Recurse
+    }
   }
 }
--- scripts/psake/removePlugin.ps1
+++ scripts/psake/removePlugin.ps1
@@ -9,14 +9,14 @@ properties {
 
   $baseDir = Resolve-Path ../..
   $publishDir = "$baseDir\plugin"
+  $publishDirPlugin1 = "$publishDir\$pluginName1"
+  $publishDirPlugin2 = "$publishDir\$pluginName2"
   $pluginPath1 =  "$baseDir\$projectName\bin\$buildConfig\$pluginName1"
   $pluginPath2 =  "$baseDir\$projectName.WinUI\bin\$buildConfig\$pluginName2"
   $appDataRoaming = $env:APPDATA
   $appDataLocal = $env:LOCALAPPDATA
   $deployDir = "$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Packages"
   $pluginFolders = @(
-    "$deployDir\$pluginName1",
-    "$deployDir\$pluginName2",
 	"$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Packages\$pluginName1",
     "$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Packages\$pluginName2",
 	"$appDataLocal\SDL\SDL Trados Studio\12\Plugins\Unpacked\$pluginFolder1",
@@ -32,13 +32,22 @@ properties {
 task default -depends Clean
 
 task Clean { 
-  Write-Host "deleting" $publishDir 
-  Remove-Item $publishDir -Force -Recurse -ErrorAction SilentlyContinue
-
+  if(Test-Path  $publishDirPlugin1){
+    Write-Host "deleting" $publishDirPlugin1 
+    Remove-Item $publishDirPlugin1 -Force -Recurse
+  }
+  
+  if(Test-Path  $publishDirPlugin2){
+    Write-Host "deleting" $publishDirPlugin2 
+    Remove-Item $publishDirPlugin2 -Force -Recurse
+  }
+  
   $pluginFolders | ForEach-Object{
     $pluginFolder = $_
-
-    Write-Host "deleting" $pluginFolder
-    Remove-Item $pluginFolder -Force -Recurse -ErrorAction SilentlyContinue 
+	
+	if(Test-Path  $pluginFolder){
+      Write-Host "deleting" $pluginFolder
+      Remove-Item $pluginFolder -Force -Recurse
+    }
   }
 }
